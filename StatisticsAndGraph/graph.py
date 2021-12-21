3884c330405dc6a1943acb2d867f2699416d2caa
jenish-rudani <jrudani1999@gmail.com>
2021-12-21 12:58:06 -0800

    Added plot generator script

---
+++ StatisticsAndGraph/graph.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from datetime import datetime
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.pyplot import figure
+figure(figsize=(18, 16), dpi=300)
+
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+
+headers = ["#Device ID", "Time (us)", "Current (mA)", "Voltage (V)",
+           "PC", "Function Name", "File Name", "Line number"]
+
+df = pd.read_csv('EnergyProfile_12-21.csv', names=headers, sep=';')
+# print(df)
+
+x = df[headers[1]][1:]
+y = df[headers[2]]
+x = pd.to_numeric(x, downcast="float")
+y = pd.to_numeric(y[1:], downcast="float")
+
+totalTimeInUs = x[len(x)]
+totalTimeInSeconds = totalTimeInUs / 1000000
+print("Total Time in Seconds : {}".format(totalTimeInSeconds))
+
+meanCurrentValue = y.mean()
+print("Average Current for this duration : {} uA/s".format(meanCurrentValue*1000))
+
+print("Peak Value for this duaration : {}".format(y.max()))
+
+x = [x[i+1]/1000000 for i in range(len(x))]
+
+plt.plot(x, y,linewidth=1)
+
+plt.title("OTA FW Update Power Consumption Profile",loc = 'left')
+plt.xlabel("Time Index (s)")
+plt.ylabel("Current (mA)")
+plt.grid(color = 'red', linestyle = '--', linewidth = 0.5)
+plt.savefig('plot.png')
+plt.show()
+
