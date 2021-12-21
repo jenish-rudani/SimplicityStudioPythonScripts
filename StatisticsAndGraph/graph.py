fe0223c71fc51bf315f84d2fe51bb60809794e8c
jenish-rudani <jrudani1999@gmail.com>
2021-12-21 13:04:06 -0800

    path errors resolved

--- StatisticsAndGraph/graph.py
+++ StatisticsAndGraph/graph.py
@@ -1,17 +1,21 @@
+import os
 import pandas as pd
 from datetime import datetime
 import csv
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.pyplot import figure
-figure(figsize=(18, 16), dpi=300)
+figure(figsize=(12, 8), dpi=300)
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+print(ROOT_DIR)
 
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
 
 headers = ["#Device ID", "Time (us)", "Current (mA)", "Voltage (V)",
            "PC", "Function Name", "File Name", "Line number"]
 
-df = pd.read_csv('EnergyProfile_12-21.csv', names=headers, sep=';')
+df = pd.read_csv('{}\EnergyProfile_12-21.csv'.format(ROOT_DIR), names=headers, sep=';')
 # print(df)
 
 x = df[headers[1]][1:]
@@ -26,7 +30,7 @@ print("Total Time in Seconds : {}".format(totalTimeInSeconds))
 meanCurrentValue = y.mean()
 print("Average Current for this duration : {} uA/s".format(meanCurrentValue*1000))
 
-print("Peak Value for this duaration : {}".format(y.max()))
+print("Peak Value for this duaration : {} mA".format(y.max()))
 
 x = [x[i+1]/1000000 for i in range(len(x))]
 
